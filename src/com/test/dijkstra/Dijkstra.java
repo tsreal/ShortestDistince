b94f5c0d5895eba1b17689a9ec3605bf0fb35dfb
tsreal <virus_tt@163.com>
2017-11-01 00:01:35 +0800

    初次提交框架

---
+++ src/com/test/dijkstra/Dijkstra.java
@@ -0,0 +1,17 @@
+package com.test.dijkstra;
+
+/*
+A       C           B       E       F           G
+A-B 14  4+9=13
+A-C 4
+A-D *   4+11=15
+A-E *   *           13+7=20
+A-F 25                      20+3=23
+A-G *   *           *       *       23+8=31
+A-H *   *           *       20+9=29             
+
+ */
+public class Dijkstra {
+
+
+}
